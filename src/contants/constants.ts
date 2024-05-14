25b4ad7c7a46d3ed230d772bab9c20d9df0829c7
safwen ben fredj <safwen.benfredj@gmail.com>
2024-05-14 16:38:09 +0200

    fixes

--- src/contants/constants.ts
+++ src/contants/constants.ts
@@ -1,2 +1,2 @@
-export const DB_URL: string = 'mongodb://localhost:27017/el-boursa';
+export const DB_URL: string = 'mongodb+srv://safwenbenfredj:k5IJofwWg2DetZg6@elboursaapi.8tzowei.mongodb.net/';
 export const TOKEN_SECRET: string = 'hard!to-guess_secret';
